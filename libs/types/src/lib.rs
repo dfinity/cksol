049a59bb960d8230ff5bce4479fff3e7d54e44a1
Louis Pahlavi <louis.pahlavi@dfinity.org>
2026-02-03 08:44:21 +0100

    Fix comment

--- libs/types/src/lib.rs
+++ libs/types/src/lib.rs
@@ -1,4 +1,4 @@
-//! Candid types used by the candid interface of the SOL RPC canister.
+//! Candid types used by the Candid interface of the ckSOL minter.
 
 #![forbid(unsafe_code)]
 #![forbid(missing_docs)]
