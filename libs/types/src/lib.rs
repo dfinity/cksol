366b123108190430eacb669ddbbf730e0ebc10f3
Maciej Modelski <maciej.modelski@dfinity.org>
2026-02-10 13:22:00 +0100

    remove unused tests reference

--- libs/types/src/lib.rs
+++ libs/types/src/lib.rs
@@ -3,9 +3,6 @@
 #![forbid(unsafe_code)]
 #![forbid(missing_docs)]
 
-#[cfg(test)]
-mod tests;
-
 use candid::{CandidType, Principal};
 use icrc_ledger_types::icrc1::account::Subaccount;
 use serde::{Deserialize, Serialize};
