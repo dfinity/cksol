5223dd1c7a80f8e465be95cd8538a4c533fc88c3
gregorydemay <gregory.demay@dfinity.org>
2026-02-13 07:24:32 +0100

    DEFI-2665: clippy

--- minter/src/lifecycle.rs
+++ minter/src/lifecycle.rs
@@ -9,9 +9,9 @@ pub fn init(args: InitArgs) {
 }
 
 pub fn post_upgrade(args: Option<UpgradeArgs>) {
-    if let Some(UpgradeArgs { deposit_fee }) = args {
-        if let Some(deposit_fee) = deposit_fee {
-            mutate_state(|s| s.deposit_fee = deposit_fee);
-        }
+    if let Some(UpgradeArgs { deposit_fee }) = args
+        && let Some(deposit_fee) = deposit_fee
+    {
+        mutate_state(|s| s.deposit_fee = deposit_fee);
     }
 }
