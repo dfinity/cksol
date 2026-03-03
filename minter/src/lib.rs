d4f7169b73eab863feb3951e4859c30a6ec35d04
Maciej Modelski <maciej.modelski@dfinity.org>
2026-03-03 08:39:11 +0100

    clippy

--- minter/src/lib.rs
+++ minter/src/lib.rs
@@ -2,12 +2,12 @@ pub mod address;
 mod guard;
 mod ledger;
 pub mod lifecycle;
-pub mod withdraw_sol;
 pub mod runtime;
 pub mod state;
 pub mod storage;
 pub mod transaction;
 pub mod update_balance;
+pub mod withdraw_sol;
 
 #[cfg(test)]
 pub mod test_fixtures;
